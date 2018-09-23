b78e83bb5f060b93d37a4cc7c89b1ab00a869dce
Justin Welch <juswelch@gmail.com>
2018-09-22 22:04:41 -0400

    make gigantor create and upload an actions file

--- snapshotgen.sh
+++ snapshotgen.sh
@@ -2,8 +2,9 @@
 
 cd $HOME
 
-# if a previous snapshot.json exists, delete it
+# if a previous snapshot or actions list exists, delete it
 rm -rf snapshot.json
+rm -rf txgen-latest.list
 
 echo snapshot-generator: installing tinman
 
@@ -16,16 +17,22 @@ pip install .
 
 cd $HOME
 
+cp $HOME/tinman/txgen.conf.example $HOME/txgen.conf
+
 timestamp=$(date +%s)
 
 echo snapshot-generator: generating a new snapshot.json file
 
 tinman snapshot -s https://api.steemit.com -o snapshot.json
 
-echo snapshot-generator: copying snapshot-$timestamp.json to s3://$S3_BUCKET
-aws s3 cp snapshot.json s3://$S3_BUCKET/snapshot-$timestamp.json
-echo snapshot-generator: copying to snapshot.json
-aws s3 cp s3://$S3_BUCKET/snapshot-$timestamp.json s3://$S3_BUCKET/snapshot.json
+echo snapshot-generator: generating a new txgen-latest.list
+
+tinman txgen -c txgen.conf -o txgen-latest.list
+
+echo snapshot-generator: copying txgen-$timestamp.json to s3://$S3_BUCKET
+aws s3 cp txgen-latest.list s3://$S3_BUCKET/txgen-$timestamp.list
+echo snapshot-generator: copying to txgen-latest.list
+aws s3 cp s3://$S3_BUCKET/txgen-$timestamp.list s3://$S3_BUCKET/txgen-latest.list
 
 echo snapshot-generator: waiting 24 hours before running again
 sleep 86400
